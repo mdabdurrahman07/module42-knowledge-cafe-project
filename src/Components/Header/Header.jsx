e4194379458b841b6e5fd14904eadc743e14aced
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-09-11 21:45:50 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="p-5 shadow-xl">
+           <h1 className="text-4xl text-left bg-white text-black font-bold">Knowledge Cafe</h1> 
+        </div>
+    );
+};
+
+export default Header;
